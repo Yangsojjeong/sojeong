518db9b47292cd469ce8140c869673accb36e75e
zoezo <zoezo@10.50.10.104>
2023-10-04 17:03:32 +0900

    add new project (HelloPackage)

---
+++ HelloPackage/src/app/GoodCal.java
@@ -0,0 +1,15 @@
+package app;
+
+import lib.Calculator;
+
+public class GoodCal extends Calculator{
+ 
+	public int add(int a, int b) {
+		return a_+ b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+}
